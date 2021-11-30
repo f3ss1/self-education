9e34a09414daa5946053159dbe4d881404aa0e0e
Sergey Zakharov <n3ffar1an@gmail.com>
2021-11-30 21:22:10 +0300

    Trash files removed.

--- Stepik/Python-bases-and-usage/test.py
+++
@@ -1,4 +0,0 @@
-import re
-MyStr = "<test>some text here</test> <other> more text </other> <test> even more text</test>"
-print(re.findall("<test>(.*?)</test>", MyStr))
-#print(re.sub(r'<.+?>', '', s))
